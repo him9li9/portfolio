d53d69f0b3b12140063e06ad95862030211c266e
Nastya Ermoshina <mr.osedax@gmail.com>
2026-06-08 19:48:24 +0300

    Disable case page HTML caching

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,28 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   devIndicators: false,
+  async headers() {
+    return [
+      {
+        source: "/work",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0"
+          }
+        ]
+      },
+      {
+        source: "/app",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0"
+          }
+        ]
+      }
+    ];
+  },
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1600, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
